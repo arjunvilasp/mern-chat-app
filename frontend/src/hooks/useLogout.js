6cd34fd0cd42a0b71fa3895e74bac808d6e6b05a
Arjun Vilas P <108401864+arjunvilasp@users.noreply.github.com>
2024-07-05 11:36:20 +0530

    Update useLogout.js

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -11,7 +11,7 @@ const useLogout = () => {
   const logout = async () => {
     setLoading(true);
     try {
-      const res = await fetch('/api/auth/logout', {
+      const res = await fetch('https://mern-chat-app-backend-p93r.onrender.com/api/auth/logout', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
       });
